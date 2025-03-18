8e6234d103a058cdd5e930a58feda75a029af9e0
viperfighterpilot33066 <vanderweertt@gmail.com>
2025-03-18 17:35:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ function updateContent(tabName) {
     `;
 
     const images1 = [
-      'images/screenshot1.png',
+      'screenshot1.png',
       'images/screenshot2.png',
       'images/screenshot3.png',
     ];
